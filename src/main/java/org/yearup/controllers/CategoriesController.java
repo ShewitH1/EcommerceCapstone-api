88fa40ebfbe7d2fa2a8fbbcbf4eddfe02a3107ff
shewit <hailu.shewit47@gmail.com>
2025-12-14 15:22:05 -0500

    web-starting-two

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -3,6 +3,7 @@ package org.yearup.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,6 +11,7 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
+@RestController
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
