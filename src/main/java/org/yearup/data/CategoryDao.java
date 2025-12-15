aac9ac2f15c9031e9fea0c7acd1d065edd9af179
shewit <hailu.shewit47@gmail.com>
2025-12-14 21:17:46 -0500

    get all/getbyid

--- src/main/java/org/yearup/data/CategoryDao.java
+++ src/main/java/org/yearup/data/CategoryDao.java
@@ -2,12 +2,13 @@ package org.yearup.data;
 
 import org.yearup.models.Category;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface CategoryDao
 {
-    List<Category> getAllCategories();
-    Category getById(int categoryId);
+    List<Category> getAllCategories() throws SQLException;
+    Category getById(int categoryId) throws SQLException;
     Category create(Category category);
     void update(int categoryId, Category category);
     void delete(int categoryId);
--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,11 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -20,15 +23,60 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public List<Category> getAllCategories()
     {
+
+        List<Category> categories = new ArrayList<>();
+
+        String query = "select * from categories";
+
         // get all categories
-        return null;
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery()) {
+
+            while(resultSet.next()){
+                int categoryid = resultSet.getInt("category_id");
+                String name = resultSet.getString("name");
+                String description = resultSet.getString("description");
+
+                Category category = new Category(categoryid, name, description);
+
+                categories.add(category);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
     @Override
-    public Category getById(int categoryId)
-    {
+    public Category getById(int categoryId) throws SQLException {
+
+        String query = """
+                select * from categories where category_id = ?
+                """;
+
         // get category by id
-        return null;
+        try(Connection connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement(query);
+            ) {
+
+            statement.setInt(1, categoryId);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int category_id = resultSet.getInt("category_id");
+                    String name = resultSet.getString("name");
+                    String description = resultSet.getString("description");
+
+                    Category category = new Category(category_id, name, description);
+
+                    return category;
+                } else {
+                    return null;
+                }
+            }
+        }
     }
 
     @Override
