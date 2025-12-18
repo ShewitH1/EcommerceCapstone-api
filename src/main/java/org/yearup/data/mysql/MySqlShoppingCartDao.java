94ff2fdd5bb5a6e3c0c45b2dbd73b204d56dc92f
shewit <hailu.shewit47@gmail.com>
2025-12-17 22:57:43 -0500

    adding pre-logic in cart file

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,40 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+
+        return null;
+    }
+
+    @Override
+    public ShoppingCart addProduct(int userId, int productId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(int userId, int productId, int quantity) throws SQLException {
+
+    }
+
+    @Override
+    public void delete(int userId, int productId) throws SQLException {
+
+    }
+
+    @Override
+    public void clearCart(int userId) throws SQLException {
+
+    }
 }
